c4f15077c4701e05df328c7edd0fc808ec7879f7
ambientcowboy <holysignals@protonmail.com>
2026-02-18 22:39:18 -0800

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace COMP003A.LectureActivity6
+{
+    class Program
+    {
+        public class Student
+        {
+            public string Name;
+            public int Age;
+
+            public Student(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {Name}, Age: {Age}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Bob", 32);
+            Student student2 = new Student("Jack", 20);
+            student1.DisplayInfo();
+            student2.DisplayInfo();
+        }
+    }
+}
